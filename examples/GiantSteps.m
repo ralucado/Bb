968b15c435fc0bcf6f429b97a00fc2dfcfa99007
klooner <victorhugola_9@hotmail.com>
2017-04-07 23:48:57 +0200

    Update GiantSteps.m

--- examples/GiantSteps.m
+++ examples/GiantSteps.m
@@ -1,20 +1,20 @@
-function maj7(Note r){
-	return (r, r + 4, r + 7, r + 11);
+Chords maj7(Note r){
+	return Chords (r, r + 4, r + 7, r + 11);
 }
 
-function dom7(Note r){
-	return (r, r + 4, r + 7, r + 10);
+Chords dom7(Note r){
+	return Chords (r, r + 4, r + 7, r + 10);
 }
 
-function min7(Note r){
-	return (r, r + 3, r + 7, r + 10);
+Chords min7(Note r){
+	return Chords (r, r + 3, r + 7, r + 10);
 }
 
-function main(){
+Chords main(){
 
 	Beat 4:4;
 
     Speed 250;
     
-	Melody chords = Melody(maj7(B).2 dom7(D).2 maj7(G).2 dom7(bB).2 maj7(bE).1 min7(A).2 dom7(D).2 maj7(G).2 dom7(bB).2 maj7(bE).2 dom7(#F).2 maj7(B).1 min7(F).2 dom7(bB).2 maj7(bE).1 min7(A).2 dom7(D).2 maj7(G).1 min7(#C).2 dom7(#F).2 maj7(B).1 min7(F).2 dom7(bB).2 maj7(bE).1 min7(#C).2 dom7(#F).2) ;
+	Melody Chords = Melody(maj7(B).2 dom7(D).2 maj7(G).2 dom7(Bb).2 maj7(Eb).1 min7(A).2 dom7(D).2 maj7(G).2 dom7(Bb).2 maj7(Eb).2 dom7(F#).2 maj7(B).1 min7(F).2 dom7(Bb).2 maj7(Eb).1 min7(A).2 dom7(D).2 maj7(G).1 min7(C#).2 dom7(F#).2 maj7(B).1 min7(F).2 dom7(Bb).2 maj7(Eb).1 min7(C#).2 dom7(F#).2) ;
 }
