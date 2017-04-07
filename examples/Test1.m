3d90d60905729174b646c43f320c4726d0c50c09
klooner <victorhugola_9@hotmail.com>
2017-04-07 22:30:29 +0200

    Update Test1.m

--- examples/Test1.m
+++ examples/Test1.m
@@ -9,7 +9,7 @@ int test2 (int i, int j){ //func type ID params LB
 void main(){
 	Speed 160;
 	(C, E, B#).8*;
-	Chord c = Chord(C, E, B#);
+	Chord C = Chord(C, E, B#);
 	for(int i = 0; i < 8; i = i + 1){
 		if(i % 2 == 0) c.8;
 		else (D, F, A).8;
